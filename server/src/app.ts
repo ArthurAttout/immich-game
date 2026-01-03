d4581c522d024e2f560b3fbff4ac87c7a7818cc6
Arthur ATTOUT <arthur.attout@outlook.com>
2026-01-03 16:44:50 +0100

    Dockerize the shit out of it

---
+++ server/src/app.ts
@@ -0,0 +1,15 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  ws.on('error', console.error);
+
+  ws.on('message', (data) => {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
+
+console.log("Listening on port 8080");
